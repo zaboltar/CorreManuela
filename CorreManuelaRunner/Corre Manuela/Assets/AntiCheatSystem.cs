5b1f1c020590948f830979dc12bfc03ec7b7b72f
ThreeGnomes <cletothar@gmail.com>
2019-08-07 08:50:44 -0500

    ranksScreen

---
+++ CorreManuelaRunner/Corre Manuela/Assets/AntiCheatSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiCheatSystem : MonoBehaviour
+{
+    // no se como hacer esto
+    /*
+    quisiera comprobar la posicion en X
+    y cuando es la misma
+    por mucho tiempo
+    es que el player se quedo enganchado en una plataforma
+    y esta ganando puntos gratis ad infinitum
+
+    -.- """
+    
+     */
+}
