ba1a52266ea45beadb66d98dfc530094c41a893c
ThreeGnomes <cletothar@gmail.com>
2019-08-09 16:24:46 -0500

    para test

---
+++ CorreManuelaRunner/Corre Manuela/Assets/GameSeer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSeer : MonoBehaviour
+{
+    public InputField usuarioNombre;
+    public Text usuarioText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SaveUserName (string txt)
+    {
+              
+        PlayerPrefs.SetString("userName", txt);
+        Debug.Log(txt);
+    }
+}
